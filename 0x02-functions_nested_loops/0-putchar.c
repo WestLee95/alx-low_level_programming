169a73813a2e3e506320e609e6835cac9faca09d
WESTLEY <eugenewestley95@gmail.com>
2023-07-18 10:43:20 -0700

    PUT2

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ *main - Entry point of the program
+ *
+ *Return: 0 on successful execution
+ */ 
+int main(void)
+{
+	write("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This handles function nested loops. These are loops within loops. For, do...while and while only.
