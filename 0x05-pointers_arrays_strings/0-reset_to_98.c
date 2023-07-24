52873300207b07d9eb7b4f412ba9583750194363
WESTLEY <eugenewestley95@gmail.com>
2023-07-24 04:33:28 -0700

    Resets-to-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+/**
+ *reset_to_98 - Resets the value to 98
+ *@n: value to be reset
+ *
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MYHEAD_H
+#define MYHEAD_H
+
+void reset_to_98(int *n);
+
+#endif
