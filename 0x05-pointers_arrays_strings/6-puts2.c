c359765d73380996b7a13de21f4c16072f258fc9
WESTLEY <eugenewestley95@gmail.com>
2023-07-24 15:23:36 -0700

    Puts_in_boots

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,17 @@
 
 
 /**
- *
- *
+ *puts2 - Prints every other character of a string
+ *@str: string to be printed
  *
  */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
