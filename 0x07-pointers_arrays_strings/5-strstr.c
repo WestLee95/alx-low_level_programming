4b5acc7f0690eb66db4b2beecb55fd6b74fc7ab4
WESTLEY <eugenewestley95@gmail.com>
2023-07-31 15:18:46 -0700

    Target_found

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,24 +4,28 @@
 /**
  *_strstr - Gets the first occurrence of the substring needle 
  *in the string haystack
- *@needle: Substring to be gotten
- *@haystack: String from which to be gotten needle
+ *@n: Substring to be gotten
+ *@h: String from which to be gotten needle
  *
  *Return: Returns a pointer to the beginning of the located substring
  *or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
+	char *h, *n;
 
 	while (*haystack)
 	{
-		for (i = 0; needle[i]; i++)
-		{
-			if (*haystack == needle[i])
-				return (haystack);
-		}
+	h = haystack;
+	n = needle;
+	while (*haystack && *n && *haystack == *n)
+	{
 		haystack++;
+		n++;
+	}
+		if (!*n && (*(haystack) == '\0' || *(n - 1) != '\0'))
+		return (h);
+	haystack = h + 1;
 	}
 
 	return (NULL);
