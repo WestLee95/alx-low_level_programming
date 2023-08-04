3459e576cfe482cd7896368f1759bf44552800b8
WESTLEY <eugenewestley95@gmail.com>
2023-08-04 16:21:56 -0700

    Add_it_up

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(*argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
