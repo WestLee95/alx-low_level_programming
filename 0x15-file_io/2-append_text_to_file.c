d737ef8e96dd049660707afdf6389b8a87fbf0c0
WestLee <eugenewestley@gmail.com>
2023-09-22 03:14:36 -0700

    Mark3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,7 +4,7 @@
 /**
  *append_text_to_file - appends text at the end of a file
  *@filename: pointer to the name of the file
- *@text_content: pointer to NULL terminated string to add at the end of the 
+ *@text_content: pointer to NULL terminated string to add at the end of the
  *file
  *Return: 1 on success and -1 on failure
  *If filename is NULL return -1
