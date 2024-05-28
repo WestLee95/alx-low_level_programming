12fc69a0b44be01fcb83fd2ff25d4bf7e8e46533
WEST <eugenewestley95@gmail.com>
2024-05-28 02:30:41 -0700

    Combine-Digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
 
-int main(void)
-{
-	int i;
-    	int j;
+int main(void) {
+    int i;
 
-	for (i = 0; i < 10; i++)
-	{
-        	for (j = 0; j < 10; j++)
-		{
-            		putchar(i + '0');
-            		putchar(',');
-            		putchar(' ');
-            		putchar(j + '0');
-            		if (!(i == 9 && j == 9))
-			{
-                		putchar(',');
-                		putchar(' ');
-            		}
-        	}
-    	}
-    	putchar('\n');
+    for (i = '0'; i <= '9'; i++) {
+        putchar(i);
+        if (i != '9') {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    putchar('\n');
 
-    	return 0;
+    return 0;
 }
