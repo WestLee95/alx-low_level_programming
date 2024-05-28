c2403a18a35c70a32c6272ddc8dc6464e98909af
WEST <eugenewestley95@gmail.com>
2024-05-28 02:25:35 -0700

    Combine_digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+    	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+        	for (j = 0; j < 10; j++)
+		{
+            		putchar(i + '0');
+            		putchar(',');
+            		putchar(' ');
+            		putchar(j + '0');
+            		if (!(i == 9 && j == 9))
+			{
+                		putchar(',');
+                		putchar(' ');
+            		}
+        	}
+    	}
+    	putchar('\n');
+
+    	return 0;
+}
